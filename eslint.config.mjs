4a3b9630f8092c549b7d67bc284b4dff2306cee7
David Fuhry <d.fuhry@tu-berlin.de>
2024-12-17 16:02:30 +0100

    Add eslint and prettier, update workspace settings, lint and format server side code

---
+++ eslint.config.mjs
@@ -0,0 +1,14 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import pluginVue from "eslint-plugin-vue";
+import eslintConfigPrettier from "eslint-config-prettier";
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  {files: ["**/*.{js,mjs,cjs,vue}"]},
+  {files: ["**/*.js"], languageOptions: {sourceType: "commonjs"}},
+  {languageOptions: { globals: {...globals.browser, ...globals.node} }},
+  pluginJs.configs.recommended,
+  ...pluginVue.configs["flat/essential"],
+  eslintConfigPrettier,
+];
