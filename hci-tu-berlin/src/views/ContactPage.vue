6ec4342aeb5b1c003e9f919790557360990b26bf
David Fuhry <d.fuhry@tu-berlin.de>
2025-01-29 15:50:53 +0100

    make things slightly less ugly

--- hci-tu-berlin/src/views/ContactPage.vue
+++ hci-tu-berlin/src/views/ContactPage.vue
@@ -21,8 +21,27 @@
         <li>Einsteinufer 17, 10587 Berlin</li>
       </ul>
     </div>
-    <div class="mt-6">
+    <!-- Do something like this, except make the consent overlay prettier 
+     (ideally  the same dimensions as the leaflet widget?) 
+     and (optionally) remember user choice in a cookie (or localstorage if you prefer)-->
+    <div class="mt-6" v-if="leafletConsent">
       <StreetMap :latitude="52.515031" :longitude="13.32682" />
+    </div>
+    <div v-else class="mt-6" style="height: 400px">
+      <img src="/assets/map_placeholder.png" style="height: 100%" />
+      <button
+        @click="leafletConsent = true"
+        class="text-xl mr-4"
+        style="
+          position: absolute;
+          top: 50%;
+          left: 50%;
+          transform: translate(-50%, -50%);
+          -ms-transform: translate(-50%, -50%);
+        "
+      >
+        Klicken um Daten von OpenStreetMap zu laden
+      </button>
     </div>
   </div>
 </template>
@@ -31,6 +50,11 @@
 import StreetMap from "@/components/StreetMap.vue";
 
 export default {
+  data() {
+    return {
+      leafletConsent: false,
+    };
+  },
   components: { StreetMap },
 };
 </script>
