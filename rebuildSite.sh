e690955dfee9348cdb081550c568e7ff797817a8
David Fuhry <d.fuhry@tu-berlin.de>
2025-02-04 11:48:27 +0100

    Revert changes to make things work

--- rebuildSite.sh
+++ rebuildSite.sh
@@ -6,11 +6,11 @@ git fetch
 git reset --hard origin/main
 git pull
 
-/home/ubuntu/.local/share/pnpm/pnpm install --force
+pnpm install --force
 
 cd /home/ubuntu/webpage/hci-tu-berlin
 
-/home/ubuntu/.local/share/pnpm/pnpm install --force
-/home/ubuntu/.local/share/pnpm/pnpm run build --emptyOutDir
+pnpm install --force
+pnpm run build --emptyOutDir
 
 sudo service hci restart
