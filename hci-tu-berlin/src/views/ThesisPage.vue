fdd5cf385f5c3fe9d3281325dff02b8a41c9532d
Tessa Billens <tessabillens@eduroam-141-23-182-238.wlan.tu-berlin.de>
2025-07-30 15:37:47 +0200

    added gitlab

--- hci-tu-berlin/src/views/ThesisPage.vue
+++ hci-tu-berlin/src/views/ThesisPage.vue
@@ -1,38 +1,53 @@
 <template>
   <div class="text-justify">
-    <h1 class="text-3xl text-left mb-8 tracking-widest">Bachelor & Master Theses</h1>
+    <h1 class="text-3xl text-left mb-8 tracking-widest">
+      Bachelor & Master Theses
+    </h1>
 
     <div class="grid grid-cols-1 md:grid-cols-2 gap-6 mb-8">
-     
       <div class="flex flex-col space-y-4">
         <p class="text-justify">
-          At the Chair of Human-Computer Interaction, we regularly supervise bachelor's and master's theses. Supervision depends on both topic fit and available capacity.
+          At the Chair of Human-Computer Interaction, we regularly supervise
+          bachelor's and master's theses. Supervision depends on both topic fit
+          and available capacity.
         </p>
         <div>
-          <h2 class="text-2xl text-left tracking-widest mb-2">Process & Application</h2>
+          <h2 class="text-2xl text-left tracking-widest mb-2">
+            Process & Application
+          </h2>
           <ul class="list-none text-left">
             <li class="mb-4">
               <h3 class="text-l mb-1 font-bold">Bachelor Theses</h3>
               <p>
-                We regularly announce an information session for interested students on this website. As long as no date is listed, we unfortunately cannot offer supervision for bachelor theses.
+                We regularly announce an information session for interested
+                students on this website. As long as no date is listed, we
+                unfortunately cannot offer supervision for bachelor theses.
               </p>
             </li>
             <li>
               <h3 class="text-l mb-1 font-bold">Master Theses</h3>
               <p>
-                If you are interested in writing a master's thesis with us, please send an email to
-                <a class="hover:text-custom-red" href="mailto:admin@hci.tu-berlin.de">admin@hci.tu-berlin.de</a>.
+                If you are interested in writing a master's thesis with us,
+                please send an email to
+                <a
+                  class="hover:text-custom-red"
+                  href="mailto:admin@hci.tu-berlin.de"
+                  >admin@hci.tu-berlin.de</a
+                >.
               </p>
               <p>Your email should include the following:</p>
               <ul class="list-disc ml-6">
                 <li>A current transcript of records</li>
-                <li>A brief motivation letter outlining your academic focus, interests and (if applicable) your own topic idea</li>
+                <li>
+                  A brief motivation letter outlining your academic focus,
+                  interests and (if applicable) your own topic idea
+                </li>
               </ul>
             </li>
           </ul>
         </div>
       </div>
-       <img
+      <img
         class="relative w-full sm:w-[70%] max-h-32rem mx-auto"
         src="/assets/hmd-photo.png"
         alt="HMD Photo"
@@ -40,17 +55,38 @@
     </div>
 
     <div>
-      <h2 class="text-2xl text-left tracking-widest mb-2">Potential Topic Areas</h2>
+      <h2 class="text-2xl text-left tracking-widest mb-2">
+        Potential Topic Areas
+      </h2>
       <ul class="list-disc ml-6 text-left">
-        <li>Usable security and authentication in immersive and smart environments</li>
-        <li>Cross-reality interaction and transitions between 2D and 3D interfaces</li>
-        <li>Interruptibility, attention management and presence in digital environments</li>
+        <li>
+          Usable security and authentication in immersive and smart environments
+        </li>
+        <li>
+          Cross-reality interaction and transitions between 2D and 3D interfaces
+        </li>
+        <li>
+          Interruptibility, attention management and presence in digital
+          environments
+        </li>
         <li>Inclusive design and technology for marginalized communities</li>
-        <li>Bystander awareness and social acceptability of emerging technologies</li>
+        <li>
+          Bystander awareness and social acceptability of emerging technologies
+        </li>
         <li>Spatial navigation and orientation support</li>
         <li>Future of work and hybrid collaboration</li>
         <li>Cultural heritage and identity in digital environments</li>
       </ul>
+      <br />
+      <p>
+        Documentation and helpful resources for thesis writing can be found in
+        our
+        <a
+          class="hover:text-custom-red"
+          href="https://git.tu-berlin.de/groups/hci"
+          >gitlab</a
+        >
+      </p>
     </div>
   </div>
 </template>
